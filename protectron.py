5786afccea0f79b49e6b13b564947560063617e9
Albert Iakupov <albert.iakupov@jibecompany.com>
2019-09-03 20:43:33 +0200

    templating

--- protectron.py
+++ protectron.py
@@ -4,9 +4,12 @@
 import asyncio
 import logging
 from enum import Enum
+from typing import Dict
 
 import shelve
 from datetime import datetime, timedelta
+from random import choices, randrange, randint
+import json
 
 # import uvloop
 
@@ -16,7 +19,8 @@ from datetime import datetime, timedelta
 from captcha.image import ImageCaptcha
 from environs import Env
 
-from random import choices, randrange
+
+from mako.template import Template
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
@@ -38,8 +42,8 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('protectron')
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher(bot)
 
 
 INPUTING = 0
@@ -56,8 +60,6 @@ class MESSAGE_TYPES(Enum):
 class PassStorage:
     def __init__(self, items, user_id, chat_id, message_id, expired_time):
         self.items = items
-        self.pos = 0
-        self.input = ''
         self.input_num = []
         self.user_id = user_id
         self.chat_id = chat_id
@@ -69,25 +71,18 @@ class PassStorage:
         self.messages[message_type] = message_id
 
     def new_char(self, ch):
-        self.input += ch
         self.input_num.append(ch)
-        self.pos += 1
-        return self.input
+        return ''.join(self.input_num)
 
     def backspace(self):
         try:
             self.input_num.pop()
         except IndexError:
             return
-        self.input = ''.join(self.input_num)
-        self.pos -= 1
-        return self.input
+        return ''.join(self.input_num)
 
     def check(self):
-        if self.pos < len(self.items):
-            # TODO: replace hardcoded to evaluate
-            # if len(self.input_num) == 4 and self.items[:4] != self.input_num:
-            #     return FAIL
+        if len(self.input_num) < len(self.items):
             return INPUTING
         log.info(f'{self.input_num} {self.items}')
         if self.input_num == self.items:
@@ -101,6 +96,7 @@ class PassStorage:
 
 
 data_store = shelve.open('data_store.db', writeback=True)
+lang_cache = {}
 
 
 async def clear(store_item):
@@ -108,20 +104,32 @@ async def clear(store_item):
         await bot.delete_message(store_item.chat_id, store_item.messages[message])
 
 
+def s(name: str, params: Dict) -> str:
+    if 'lang' not in params:
+        log.error(f'Missing required lang on {params}')
+        return ''
+    lang = params['lang']
+    if lang not in lang_cache:
+        with open(f'templates/{lang}.json', 'r') as template:
+            lang_cache[lang] = json.loads(template.read())
+    # TODO: check if it is a list
+    template = lang_cache[lang][name]
+    return Template(template).render(**params)
+
+
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('backspace'))
 async def process_callback_backspace(callback_query: types.CallbackQuery):
-    debug_id = f'{callback_query.message.chat.username}-(@{callback_query.from_user.username})'\
-        f'{callback_query.from_user.first_name} {callback_query.from_user.last_name}'
+    debug_id = f'{callback_query.message.chat.username}-({callback_query.from_user.mention})'
     log.info(f'{debug_id}: backspace')
     _id = f'{callback_query.message.message_id}-{callback_query.message.chat.id}'
     try:
         pass_item = data_store[_id]
     except KeyError:
-        log.error(f'Something gone wrong: {data_store["store"]} {_id}')
-        await bot.answer_callback_query(callback_query.id, text='Упс. что то пошло не так')
+        log.error(f'Something gone wrong: {data_store} {_id}')
+        await bot.answer_callback_query(callback_query.id, text=s('something_gone_wrong_warn', {'lang': 'ru'}))
         return
     if not pass_item.user_check(callback_query.from_user.id):
-        await bot.answer_callback_query(callback_query.id, text='Это не для тебя.')
+        await bot.answer_callback_query(callback_query.id, text=s('not_for_you_warn', {'lang': 'ru'}))
         return
     text = pass_item.backspace()
     await bot.answer_callback_query(callback_query.id, text=text)
@@ -130,21 +138,21 @@ async def process_callback_backspace(callback_query: types.CallbackQuery):
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('btn'))
 async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
     code = callback_query.data
-    chat_id = callback_query['message']['chat']['id']
-    member_id = callback_query['from']['id']
-    user_title = f'{callback_query["from"]["first_name"]} {callback_query["from"]["last_name"]}'
-    debug_id = f'{callback_query["message"]["chat"]["username"]}-(@{callback_query["from"]["username"]}){user_title}'
+    chat_id = callback_query.message.chat.id
+    member_id = callback_query.from_user.id
+    user_title = callback_query.from_user.mention
+    debug_id = f'{callback_query.message.chat.username}-({user_title})'
+    chat_title = callback_query.message.chat.title
     log.info(f'{debug_id}: {code}')
-    _id = str(callback_query['message']['message_id']) + \
-        '-' + str(callback_query['message']['chat']['id'])
+    _id = f'{callback_query.message.message_id}-{callback_query.message.chat.id}'
     try:
         pass_item = data_store[_id]
     except KeyError:
-        log.error(f'Something gone wrong: {data_store["store"]} {_id}')
-        await bot.answer_callback_query(callback_query.id, text='Упс. что то пошло не так')
+        log.error(f'Something gone wrong: {data_store} {_id}')
+        await bot.answer_callback_query(callback_query.id, text=s('something_gone_wrong_warn', {'lang': 'ru'}))
         return
-    if not pass_item.user_check(callback_query['from']['id']):
-        await bot.answer_callback_query(callback_query.id, text='Это не для тебя.')
+    if not pass_item.user_check(callback_query.from_user.id):
+        await bot.answer_callback_query(callback_query.id, text=s('not_for_you_warn', {'lang': 'ru'}))
         return
     text = pass_item.new_char(code[-1:])
     result = pass_item.check()
@@ -156,11 +164,8 @@ async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
         await bot.answer_callback_query(callback_query.id, text='SUCSSES')
         pass_item.messages.pop(MESSAGE_TYPES.LOGIN)
         await clear(pass_item)
-        await bot.send_message(callback_query['message']['chat']['id'],
-                               f'''{user_title}, добро пожаловать в {callback_query.message.chat.title}!
-Здесь нет:
-- политики, хамства и троллей
-- рекламы без одобрения админа''')
+        await bot.send_message(callback_query.message.chat.id,
+                               s('success_msg', {'lang': 'ru', 'user_title': user_title, 'chat_title': chat_title}))
         unmute = ChatPermissions(can_send_messages=True,
                                  can_send_media_messages=True,
                                  can_add_web_page_previews=True,
@@ -170,7 +175,7 @@ async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
     else:
         data_store.pop(_id)
         log.info(f'{debug_id}: FAIL')
-        await bot.answer_callback_query(callback_query.id, text='FAIL')
+        await bot.answer_callback_query(callback_query.id, text=s('fail_msg', {'lang': 'ru'}))
         await clear(pass_item)
         await bot.kick_chat_member(chat_id, member_id)
         await bot.unban_chat_member(chat_id, member_id)
@@ -182,11 +187,11 @@ async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
 async def leave_event(message: types.Message):
     for _id in data_store:
         pass_item = data_store[_id]
-        if pass_item.chat_id == message.chat.id and pass_item.user_id == message['left_chat_member']['id']:
+        if pass_item.chat_id == message.chat.id and pass_item.user_id == message.left_chat_member.id:
             log.info(
                 f'{pass_item.chat_id}:@{pass_item.user_id}: Left chat, clean')
             pass_item = data_store.pop(_id)
-            pass_item.add_message_id(MESSAGE_TYPES.LEFT, message['message_id'])
+            pass_item.add_message_id(MESSAGE_TYPES.LEFT, message.message_id)
             await clear(pass_item)
             data_store.sync()
             return
@@ -196,7 +201,7 @@ async def leave_event(message: types.Message):
 async def ping(message: types.Message):
     log.info(f'Ping requsted {message.chat.title}!')
     await bot.send_message(message.chat.id,
-                           text='pong!\nI\'m still here.',
+                           text=s('pong_msg', {'lang': 'ru'}),
                            reply_to_message_id=message.message_id)
 
 
@@ -213,20 +218,18 @@ async def capcha(message: types.Message):
             continue
         if member.is_bot:
             await bot.send_message(message.chat.id,
-                                   text='Привет новый бот!',
+                                   text=s('join_bot_msg', {'lang': 'ru'}),
                                    reply_to_message_id=message.message_id)
             continue
         # mute user
-        debug_id = f'{message.chat.username}-@{member.username}'
+        user_title = member.mention
+        debug_id = f'{message.chat.username}-{user_title}'
         log.info(f'{debug_id}: Start capcha')
         await bot.restrict_chat_member(message.chat.id, member.id, permissions=mute)
         inline_kb_full = InlineKeyboardMarkup(row_width=4)
-        # btn = InlineKeyboardButton('Вторая кнопка', callback_data='btn2')
-        # inline_kb_full.add(btn, btn, btn, btn, btn, btn, btn)
         # captcha_text_store = 'あかさたなはまやらわがざだばぴぢじぎりみひにちしきぃうぅくすつぬふむゆゅるぐずづぶぷぺべでぜげゑれめねてへせけぇえおこそとのほもよょろをごぞどぼぽ、ゞゝんっゔ'
         captcha_text_store = 'asdfghjkzxcvbnmqwertyu2345678'
         captcha_text = choices(captcha_text_store, k=8)
-        # with pysnooper.snoop():
         btn_text = list(captcha_text)
         btn_pass = list(btn_text)
         btn_order = []
@@ -253,23 +256,25 @@ async def capcha(message: types.Message):
         image_captcha = ImageCaptcha(
             fonts=['NotoSansCJKjp-Regular.otf'],  width=350, height=200)
         image = image_captcha.generate_image(captcha_text)
-        image_captcha.create_noise_curve(image, image.getcolors())
+        for _ in range(randint(1, 5)):
+            image_captcha.create_noise_curve(image, image.getcolors())
 
         # Add noise dots for the image.
         image_captcha.create_noise_dots(image, image.getcolors())
         input_file = InputFile(image_captcha.generate(captcha_text))
 
         sent_message = await bot.send_photo(message.chat.id, input_file,
-                                            caption='Magic buttons\nЕсли хочешь жить умей вертеться. У тебя 5 минут.',
+                                            caption=s(
+                                                'join_msg', {'lang': 'ru', 'user_title': user_title}),
                                             reply_markup=inline_kb_full,
                                             reply_to_message_id=message.message_id)
-        _id = f'{sent_message["message_id"]}-{sent_message["chat"]["id"]}'
+        _id = f'{sent_message.message_id}-{sent_message.chat.id}'
         expired_time = datetime.now() + timedelta(minutes=5)
         data_store[_id] = PassStorage(
-            btn_pass, member.id, sent_message["chat"]["id"], sent_message["message_id"], expired_time)
+            btn_pass, member.id, sent_message.chat.id, sent_message.message_id, expired_time)
         data_store[_id].add_message_id(MESSAGE_TYPES.LOGIN, message.message_id)
         data_store[_id].add_message_id(
-            MESSAGE_TYPES.CAPTCHA, sent_message["message_id"])
+            MESSAGE_TYPES.CAPTCHA, sent_message.message_id)
         data_store.sync()
 
 
